9e9348825d7e4abf72f952c2159f7815995055f5
Wei Cong <weicong317@gmail.com>
2018-08-12 19:01:49 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
   root "homepage#index"
   
+  get "admin/dashboard" => "admin#dashboard"
+
+  resources :user, controller: "users", only: [:new, :create, :show]
+  
+  get "/auth/:provider/callback" => "sessions#create_with_omniauth"
+  
+  resources :sessions, only: [:new, :create, :destroy]
+
   get "/messages/orderByOldToNew" => "messages#old_to_new"
   get "/messages/orderByWithComment" => "messages#with_comment"
   get "/messages/orderByWithImage" => "messages#with_image"
   get "/messages/search" => "messages#search"
   post "/messages/delete_request/:id" => "messages#delete_request"
-  put "/messages/report/:id" => "messages#report"
 
-  get "admin/dashboard" => "admin#dashboard"
-
-  
-  resources :user, controller: "users"
-  resources :sessions
-  resources :messages do
-    resources :reports
+  resources :messages, except: [:edit] do
+    resources :reports, only: [:create, :destroy]
   end
-  resources :comments
 
-  get "/auth/:provider/callback" => "sessions#create_with_omniauth"
+  resources :comments, only: [:create, :destroy]
+
 end
